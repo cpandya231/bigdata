a1ff8ae6a22b7ecf1b720a175741231e10a0a8a2
cpandya231 <chintan.pandya@ezest.in>
2019-09-15 22:39:34 +0530

    Update config

--- SparkBeginner2/src/main/scala/DataframeExample.scala
+++ SparkBeginner2/src/main/scala/DataframeExample.scala
@@ -1,7 +1,8 @@
 
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, window}
-
+import org.apache.spark.sql.types.{StructField, StructType, StringType, LongType}
 
 object DataframeExample {
 
@@ -14,13 +15,36 @@ object DataframeExample {
       .getOrCreate()
     spark.conf.set("spark.sql.shuffle.partitions", "5")
 
-    import spark.implicits._
-    val column= Seq(("hii",2,1l)).toDF("col1","col2","col3")
-    column.show(10)
 
+    val myManualSchema = new StructType(Array(
+      new StructField("DEST_COUNTRY_NAME", StringType, true),
+      new StructField("ORIGIN_COUNTRY_NAME", StringType, true),
+      new StructField("count", LongType, false)
+    ))
+    //Reading from a CSV file
     val retailData=spark.read.format("csv").option("header","true").option("inferSchema","true")
-      .load("/home/chintan/Downloads/spark-2.4.0-bin-hadoop2.7/data/Spark-The-Definitive-Guide/data/retail-data/by-day/*.csv")
+      .load("/home/chintan/Downloads/spark-2.4.0-bin-hadoop2.7/data/Spark-The-Definitive-Guide/data/flight-data/csv/2010-summary.csv")
 
     retailData.show(10)
+
+
+
+    //Reading from a json file
+    val retailDataJson=spark.read.format("json").option("mode","FAILFAST").schema(myManualSchema)
+      .load("/home/chintan/Downloads/spark-2.4.0-bin-hadoop2.7/data/Spark-The-Definitive-Guide/data/flight-data/json/2010-summary.json")
+    retailDataJson.show(10)
+
+    //To write csv data in json
+    retailData.write.format("json").mode("overwrite").save("/tmp/my-json-file.json")
+
+    //Parquet file
+    val retailDataParquet=spark.read.format("parquet")
+      .load("/home/chintan/Downloads/spark-2.4.0-bin-hadoop2.7/data/Spark-The-Definitive-Guide/data/flight-data/parquet/2010-summary.parquet")
+    retailDataParquet.show(10)
+
+    //Orc file
+    val retailDataOrc=spark.read.format("orc")
+      .load("/home/chintan/Downloads/spark-2.4.0-bin-hadoop2.7/data/Spark-The-Definitive-Guide/data/flight-data/orc/2010-summary.orc")
+    retailDataOrc.show(10)
   }
 }
---
+++ SparkBeginner2/src/main/scala/ReadingFromDBExample.scala
@@ -0,0 +1,42 @@
+
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
+import  java.sql.DriverManager
+object ReadingFromDBExample {
+
+
+
+  def main(args: Array[String]): Unit = {
+
+
+  val spark = SparkSession.builder().master("local")
+      .getOrCreate()
+    spark.conf.set("spark.sql.shuffle.partitions", "5")
+
+    val driver = "org.sqlite.JDBC"
+    val path = "/home/chintan/Downloads/spark-2.4.0-bin-hadoop2.7/data/Spark-The-Definitive-Guide/data/flight-data/jdbc/my-sqlite.db"
+    val url = s"jdbc:sqlite:/${path}"
+    val tablename = "flight_info"
+
+    //To test connection
+//    val connection= DriverManager.getConnection(url);
+//    connection.isClosed()
+
+    val dbDataFrame= spark.read.format("jdbc").option("url",url).option("dbtable",tablename).option("driver",driver).load()
+    print("Any")
+    dbDataFrame.select("DEST_COUNTRY_NAME").distinct().show(5)
+
+    // Reading data by partitioning predicates
+
+    val props = new java.util.Properties
+    props.setProperty("driver","org.sqlite.JDBC")
+
+    val predicates = Array(
+      "DEST_COUNTRY_NAME = 'Sweden' OR ORIGIN_COUNTRY_NAME = 'Sweden'",
+      "DEST_COUNTRY_NAME = 'Anguilla' OR ORIGIN_COUNTRY_NAME = 'Anguilla'")
+
+    spark.read.jdbc(url,tablename,predicates,props).show()
+    println(spark.read.jdbc(url, tablename, predicates, props).rdd.getNumPartitions)
+  }
+}
